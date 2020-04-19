8c56fe559ccce7d0859550458f067b3b36fc06d3
未央 <hiveforwork@163.com>
2020-04-19 22:23:42 +0800

    初始化提交

---
+++ src/main/java/com/jnxaread/JnxareadCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jnxaread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnxareadCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnxareadCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jnxaread/JnxareadCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnxaread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnxareadCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
