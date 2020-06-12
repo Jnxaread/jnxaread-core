2feefe02b285329393cac43b47c5babae8ed93d3
李嵩 <lisong@pansoft.com>
2020-06-12 16:35:10 +0800

    作品信息查询新增作品最大章节号查询

--- src/main/java/com/jnxaread/bean/wrap/FictionWrap.java
+++ src/main/java/com/jnxaread/bean/wrap/FictionWrap.java
@@ -1,6 +1,7 @@
 package com.jnxaread.bean.wrap;
 
 import com.jnxaread.bean.Fiction;
+import lombok.Data;
 
 import java.util.Date;
 
@@ -8,6 +9,7 @@ import java.util.Date;
  * @author 未央
  * @create 2020-05-07 10:02
  */
+@Data
 public class FictionWrap extends Fiction {
 
     private String category;
@@ -22,51 +24,5 @@ public class FictionWrap extends Fiction {
 
     private String[] tags;
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Date getLastTime() {
-        return lastTime;
-    }
-
-    public void setLastTime(Date lastTime) {
-        this.lastTime = lastTime;
-    }
-
-    public Integer getLastNumber() {
-        return lastNumber;
-    }
-
-    public void setLastNumber(Integer lastNumber) {
-        this.lastNumber = lastNumber;
-    }
-
-    public String getLastChapter() {
-        return lastChapter;
-    }
-
-    public void setLastChapter(String lastChapter) {
-        this.lastChapter = lastChapter;
-    }
-
-    public String[] getTags() {
-        return tags;
-    }
-
-    public void setTags(String[] tags) {
-        this.tags = tags;
-    }
+    private Integer maxNumber;
 }
--- src/main/java/com/jnxaread/dao/ChapterMapper.java
+++ src/main/java/com/jnxaread/dao/ChapterMapper.java
@@ -41,4 +41,6 @@ public interface ChapterMapper {
     /******************************************************************************************************************/
 
     ChapterWrap findWithUsername(int id);
+
+    int selectMaxNumberByFictionId(int fictionId);
 }
