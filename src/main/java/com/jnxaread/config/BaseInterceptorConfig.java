68b06aea49ecc771e07fa7c8d6f0c60b63082196
未央 <hiveforwork@163.com>
2020-06-19 19:06:56 +0800

    恢复登陆校验拦截器配置

--- src/main/java/com/jnxaread/config/BaseInterceptorConfig.java
+++ src/main/java/com/jnxaread/config/BaseInterceptorConfig.java
@@ -1,6 +1,7 @@
 package com.jnxaread.config;
 
 import com.jnxaread.interceptor.AccessOriginInterceptor;
+import com.jnxaread.interceptor.LoginCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,8 +17,19 @@ public class BaseInterceptorConfig implements WebMvcConfigurer {
     @Autowired
     private AccessOriginInterceptor accessOriginInterceptor;
 
+    @Autowired
+    private LoginCheckInterceptor loginCheckInterceptor;
+
+    private String[] loginCheckPath = {
+            "/**/new/**", //所有的创建作品的路径
+            "/**/own", //所有的请求自己的作品的路径
+            "/**/edit/**", //所有的编辑或修改路径
+            "/**/brief/chapter", //获取章节简要信息的路径
+    };
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(accessOriginInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(loginCheckInterceptor).addPathPatterns(loginCheckPath);
     }
 }
