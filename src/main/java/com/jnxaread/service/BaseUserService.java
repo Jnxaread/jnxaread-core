d36014239c4a84b26d246eff3b90ae0cf4cff3ec
李嵩 <lisong@pansoft.com>
2020-06-24 09:59:01 +0800

    新增业务层获取用户列表方法

--- src/main/java/com/jnxaread/service/BaseUserService.java
+++ src/main/java/com/jnxaread/service/BaseUserService.java
@@ -3,6 +3,8 @@ package com.jnxaread.service;
 import com.jnxaread.bean.Login;
 import com.jnxaread.bean.User;
 
+import java.util.List;
+
 /**
  * @author 未央
  * @create 2020-05-02 17:01
@@ -12,6 +14,8 @@ public interface BaseUserService {
     User addUser(User newUser);
 
     User getUser(int id);
+
+    List<User> getUserList();
 
     User getUserByAccount(String account);
 
