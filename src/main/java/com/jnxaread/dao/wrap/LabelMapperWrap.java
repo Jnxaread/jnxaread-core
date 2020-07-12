7cd06336679ae1dd4c5a43b3840a5cc7c64018d1
未央 <hiveforwork@163.com>
2020-07-12 11:32:21 +0800

    修复某些小问题

--- src/main/java/com/jnxaread/dao/wrap/LabelMapperWrap.java
+++ src/main/java/com/jnxaread/dao/wrap/LabelMapperWrap.java
@@ -1,10 +1,12 @@
 package com.jnxaread.dao.wrap;
 
 import com.jnxaread.dao.LabelMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author 未央
  * @create 2020-07-11 17:31
  */
+@Mapper
 public interface LabelMapperWrap extends LabelMapper {
 }
