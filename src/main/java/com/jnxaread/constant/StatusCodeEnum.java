2b5657838b8486636277251899eeedfd26633db1
LiSong-ux <546993317@qq.com>
2021-01-30 17:40:07 +0800

    新增某些状态码；用户注册成功后从emailMap中删除该用户的键值

--- src/main/java/com/jnxaread/constant/StatusCodeEnum.java
+++ src/main/java/com/jnxaread/constant/StatusCodeEnum.java
@@ -7,7 +7,13 @@ package com.jnxaread.constant;
 public enum StatusCodeEnum {
     OK("000000", "执行成功"),
     PASS_CHECK_FORBIDDEN("010001", "禁止通行"),
-    PASS_CHECK_LIMITED("010002", "禁止通行");
+    PASS_CHECK_LIMITED("010002", "禁止通行"),
+    PARAMETER_INVALIDATE("010003", "参数错误"),
+    ACCOUNT_OR_PASSWORD_INVALIDATE("010004", "账号或密码错误"),
+    NOT_LOGGED_IN("010010", "用户未登录"),
+
+
+    EXECUTION_TIMEOUT("020100", "系统执行超时");
 
 
     private final String code;
