ded02f4113eebaf90fa3647c670312cf032ed1b3
LiSong-ux <546993317@qq.com>
2021-01-31 12:22:18 +0800

    新增工程信息管理组件（service），添加了些注释

---
+++ src/main/java/com/jnxaread/service/BaseProjectService.java
@@ -0,0 +1,8 @@
+package com.jnxaread.service;
+
+/**
+ * @Author 未央
+ * @Create 2021-01-31 12:14
+ */
+public interface BaseProjectService {
+}
---
+++ src/main/java/com/jnxaread/service/impl/BaseProjectServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jnxaread.service.impl;
+
+/**
+ * @Author 未央
+ * @Create 2021-01-31 12:15
+ */
+public class BaseProjectServiceImpl {
+}
